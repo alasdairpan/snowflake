7d8ecae7accc5cc9daf1573a80441c665f78d892
Trayvon Pan <trayvonpan@gmail.com>
2024-09-07 23:14:25 +0800

    refactor: improved performance and readability

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,39 @@
 //! Snowflake is a unique ID generator that generates IDs based on the current
-//! time, a node ID, and a step value.
+//! time, a worker ID, and a sequence value.
 //!
-//! The Snowflake struct represents a snowflake generator and provides methods
-//! for generating snowflake IDs.
+//! Snowflake IDs are not strictly sequential but are designed to be ordered.
+//! The ordering is based on the timestamp part of the ID, so IDs generated at
+//! later times will be numerically larger than those generated earlier.
+//! However, within the same millisecond, IDs can vary based on the sequence
+//! number. Here are key points about ordering:
+//! - **Temporal Ordering**: IDs are ordered by the timestamp portion, so IDs
+//!   generated at different times are in chronological order.
+//! - **Same Timestamp**: When multiple IDs are generated within the same
+//!   millisecond, the sequence number differentiates them. This ensures
+//!   uniqueness but doesn't guarantee a strict numerical order within that
+//!   millisecond.
+//! - **Clock Skew**: If system clocks are not synchronized across workers, or
+//!   if a machine's clock goes backward, it might lead to IDs that don't
+//!   strictly follow the expected order.
+//!
+//! Default Snowflake ID structure:
+//! - **Sign bit**: Always 0.
+//! - **Timestamp**: 41 bits, representing milliseconds since a custom epoch.
+//! - **Worker ID**: 10 bits, identifying the worker that generated the ID.
+//! - **Sequence**: 12 bits, providing uniqueness within the same millisecond.
+//! - **Total**: 64 bits.
+//!
+//! You can customize the number of bits used for the worker ID and sequence
+//! number. The total number of bits must be 64, and the worker ID and sequence
+//! number must be at least 1 bit each.
+//!
 //!
 //! # Examples
 //!
 //! ```
 //! use snowflake::Snowflake;
 //!
-//! // Create a new snowflake generator with a node ID
+//! // Create a new snowflake generator with a worker ID
 //! let mut snowflake = Snowflake::new(1).unwrap();
 //!
 //! // Generate a snowflake ID
@@ -21,229 +45,173 @@
 //!
 //! The Snowflake generator can return the following errors:
 //!
-//! - `ArgumentError`: Indicates an invalid argument was provided to the
-//!   Snowflake generator.
-//! - `ClockMoveBackwards`: Indicates that the system clock has moved backwards.
-//! - `WaitForNextSecondTimeout`: Indicates that waiting for the next second has
-//!   timed out.
-//! - `EpochEarlierThanCurrentTime`: Indicates that the epoch time is earlier
-//!   than the current time.
+//! - [`ArgumentError`](SnowflakeError::ArgumentError): Indicates an invalid
+//!   argument was provided to the Snowflake generator.
+//! - [`ClockMoveBackwards`](SnowflakeError::ClockMoveBackwards): Indicates that
+//!   the system clock has moved backwards.
+//! - [`WaitForNextPeriodTimeout`](SnowflakeError::WaitForNextPeriodTimeout):
+//!   Indicates that the generator has timed out while waiting for the next time
+//!   period.
+//! - [`InvalidEpoch`](SnowflakeError::InvalidEpoch): Indicates that the epoch
+//!  time must be greater than the current time.
+//! - [`FailedConvertToMillis`](SnowflakeError::FailedConvertToMillis):
+//!   Indicates that the generator failed to convert the timestamp to
+//!   milliseconds.
+//!
 //!
 //! # Safety
 //!
 //! The Snowflake generator is safe to use in a multi-threaded environment as
 //! long as each thread has its own instance of the generator.
-//!
-//! # Notes
-//!
-//! - The Snowflake generator uses a combination of the current time, a node ID,
-//!   and a step value to generate unique IDs.
-//! - The node ID should be unique for each node in a distributed system to
-//!   avoid ID collisions.
-//! - The step value is incremented for each ID generated within the same time
-//!   period to ensure uniqueness.
-//! - The Snowflake generator has a timeout duration for waiting for the next
-//!   time period, which can be configured during initialization.
-//! - The epoch time is used as a reference for generating snowflake IDs and
-//!   should be set to a time before the current time.
-//! - The Snowflake generator supports a maximum of 256 nodes (2^8) and a
-//!   maximum of 16777215 steps (2^24 - 1) within a time period.
-//! - The Snowflake generator guarantees that generated IDs will be unique as
-//!   long as the node ID and step value are unique within a time period.
-//! - The Snowflake generator does not guarantee the order of generated IDs, as
-//!   the step value can be incremented multiple times within the same time
-//!   period.
-//! - The Snowflake generator is not suitable for generating sequential IDs or
-//!   for use cases that require strict ordering of IDs.
-//! - The Snowflake generator is designed to be fast and efficient, using
-//!   bitwise operations and system time functions for ID generation.
-//! - The Snowflake generator is inspired by Twitter's Snowflake ID generation
-//!   algorithm.
 
 use std::{
     cmp::Ordering,
     hint::spin_loop,
-    time::{Duration, Instant, SystemTime, UNIX_EPOCH},
+    time::{Instant, SystemTime, UNIX_EPOCH},
 };
 
-const NODE_BITS: u8 = 8;
-const MIN_BITS: u8 = 1;
-const TOTAL_BITS: u8 = 31;
+const EPOCH: u64 = 1704038400000; // 2024-01-01 00:00:00.000
+const SIGN_BITS: u64 = 1;
+const TIMESTAMP_BITS: u64 = 41;
+const WORKER_ID_BITS: u64 = 10;
+
 const TIMEOUT_MILLIS: u128 = 1000;
-const EPOCH: u64 = 1704038400; // 2024-01-01 00:00:00
 
-#[derive(Debug, Clone)]
+const MIN_BITS: u64 = 1;
+const MAX_ADJUSTABLE_BITS: u64 = 64 - SIGN_BITS - TIMESTAMP_BITS;
+
+#[derive(Debug)]
 pub struct Snowflake {
-    epoch: SystemTime, // The epoch time used as a reference for generating snowflakes
-    time: i64,         // The most recent generation time
-    node_id: i64,      // The ID of the node generating the snowflakes
-    step: i64,         // The step within a time period
+    epoch: u64,          // The epoch time used as a reference
+    last_timestamp: u64, // The most recent generation time
+    worker_id: u64,      // The ID of the worker generating the snowflakes
+    sequence: u64,       // The sequence within a time period
 
-    step_mask: i64, // A mask to limit the step value within the allowed range
-    time_shift: u8, // The number of bits to shift the time value
-    node_shift: u8, // The number of bits to shift the node ID value
+    max_sequence: u64,    // A mask to limit the sequence value within the allowed range
+    timestamp_shift: u64, // The number of bits to shift the time value
+    worker_id_shift: u64, // The number of bits to shift the worker ID value
 
     timeout_millis: u128, // The timeout duration for waiting for the next time period
 }
 
 #[derive(thiserror::Error, Debug, Clone, PartialEq)]
 pub enum SnowflakeError {
-    #[error("argument error, expect: {0}")]
+    #[error("argument error: {0}")]
     ArgumentError(String),
     #[error("clock move backwards")]
     ClockMoveBackwards,
-    #[error("waiting for the next second has timed out")]
-    WaitForNextSecondTimeout,
-    #[error("epoch must be less than the current time")]
-    EpochEarlierThanCurrentTime,
+    #[error("waiting for the next period has timed out")]
+    WaitForNextPeriodTimeout,
+    #[error("epoch must be greater than the current time")]
+    InvalidEpoch,
+    #[error("failed to convert timestamp to milliseconds")]
+    FailedConvertToMillis,
 }
 
 impl Snowflake {
-    pub fn new(node_id: i64) -> Result<Self, SnowflakeError> {
-        Self::with_config(node_id, NODE_BITS, TIMEOUT_MILLIS, EPOCH)
+    pub fn new(worker_id: u64) -> Result<Self, SnowflakeError> {
+        Self::with_config(worker_id, WORKER_ID_BITS, TIMEOUT_MILLIS, EPOCH)
     }
 
-    pub fn with_config(node_id: i64, node_bits: u8, timeout_millis: u128, epoch: u64) -> Result<Self, SnowflakeError> {
-        let step_bits = TOTAL_BITS - node_bits;
-        if node_bits < MIN_BITS || step_bits < MIN_BITS {
+    pub fn with_config(
+        worker_id: u64,
+        worker_id_bits: u64,
+        timeout_millis: u128,
+        epoch: u64,
+    ) -> Result<Self, SnowflakeError> {
+        if !(MIN_BITS .. MAX_ADJUSTABLE_BITS).contains(&worker_id_bits)
+            || !(MIN_BITS .. MAX_ADJUSTABLE_BITS).contains(&(MAX_ADJUSTABLE_BITS - worker_id_bits))
+        {
+            return  Err(SnowflakeError::ArgumentError(format!(
+                    "invalid worker id bits(={worker_id_bits}), expected worker id bits ∈ [{MIN_BITS},{MAX_ADJUSTABLE_BITS})",
+                )));
+        }
+
+        let sequence_bits = MAX_ADJUSTABLE_BITS - worker_id_bits;
+        let max_worker_id = (1 << worker_id_bits) - 1;
+        let max_sequence = (1 << sequence_bits) - 1;
+        let worker_id_shift = sequence_bits;
+        let timestamp_shift = worker_id_bits + sequence_bits;
+
+        if worker_id > max_worker_id {
             return Err(SnowflakeError::ArgumentError(format!(
-                "{} <= node bits <= {}",
-                MIN_BITS,
-                TOTAL_BITS - MIN_BITS
+                "invalid worker id(={worker_id}), expected worker id ∈ [0,{max_worker_id}]",
             )));
         }
 
-        let node_max = -1 ^ (-1 << node_bits);
-        let step_mask = -1 ^ (-1 << step_bits);
-        let time_shift = node_bits + step_bits;
-        let node_shift = step_bits;
-
-        if node_id < 0 || node_id > node_max {
-            return Err(SnowflakeError::ArgumentError(format!("0 <= node id <= {node_max}")));
+        if epoch >= Self::timestamp_millis()? {
+            return Err(SnowflakeError::InvalidEpoch);
         }
 
-        let adjusted_epoch = UNIX_EPOCH + Duration::from_secs(epoch);
-        match SystemTime::now().duration_since(adjusted_epoch) {
-            Err(_) => Err(SnowflakeError::ArgumentError(
-                "epoch must be less than the current time".to_string(),
-            )),
-            Ok(dur) => Ok(Self {
-                epoch: adjusted_epoch,
-                time: dur.as_secs() as i64,
-                node_id,
-                step: 0,
-                timeout_millis,
-                step_mask,
-                time_shift,
-                node_shift,
-            }),
-        }
+        Ok(Self {
+            epoch,
+            last_timestamp: 0,
+            worker_id,
+            sequence: 0,
+            timeout_millis,
+            max_sequence,
+            timestamp_shift,
+            worker_id_shift,
+        })
     }
 
-    pub fn generate(&mut self) -> Result<i64, SnowflakeError> {
-        let duration_since = |epoch| SystemTime::now().duration_since(epoch);
-        match duration_since(self.epoch) {
-            Ok(dur) => {
-                let mut now = dur.as_secs() as i64;
-                match now.cmp(&self.time) {
-                    // Clock move backwards
-                    Ordering::Less => {
-                        let possible_step = (self.step + 1) & self.step_mask;
-                        if possible_step > 0 {
-                            // Continue to use the remaining step in the last time period
-                            self.step = possible_step;
-                            return Ok((self.time << self.time_shift)
-                                | (self.node_id << self.node_shift)
-                                | (self.step));
+    pub fn generate(&mut self) -> Result<u64, SnowflakeError> {
+        let mut now = self.current_timestamp_millis_since_epoch()?;
+        match now.cmp(&self.last_timestamp) {
+            // Clock move backwards
+            Ordering::Less => {
+                let possible_sequence = (self.sequence + 1) & self.max_sequence;
+                if possible_sequence > 0 {
+                    // Continue to use the remaining sequence in the last time period
+                    self.sequence = possible_sequence;
+                    return Ok((self.last_timestamp << self.timestamp_shift)
+                        | (self.worker_id << self.worker_id_shift)
+                        | (self.sequence));
+                }
+                return Err(SnowflakeError::ClockMoveBackwards);
+            }
+            // Multiple calls within the same time period can increase sequence
+            Ordering::Equal => {
+                self.sequence = (self.sequence + 1) & self.max_sequence;
+                if self.sequence == 0 {
+                    // The sequence of the current period has been used up, waiting for the next
+                    // period
+                    let timeout_start = Instant::now();
+                    while now <= self.last_timestamp {
+                        if Instant::now().duration_since(timeout_start).as_millis() > self.timeout_millis {
+                            return Err(SnowflakeError::WaitForNextPeriodTimeout);
                         }
-                        return Err(SnowflakeError::ClockMoveBackwards);
-                    }
-                    // Multiple calls within the same time period can increase step
-                    Ordering::Equal => {
-                        self.step = (self.step + 1) & self.step_mask;
-                        if self.step == 0 {
-                            // The step of the current period has been used up, waiting for the next period
-                            let timeout_start = Instant::now();
-                            while now <= self.time {
-                                if Instant::now().duration_since(timeout_start).as_millis() > self.timeout_millis {
-                                    return Err(SnowflakeError::WaitForNextSecondTimeout);
-                                }
-                                if let Ok(dur) = duration_since(self.epoch) {
-                                    now = dur.as_secs() as i64;
-                                }
-                                spin_loop();
-                            }
+                        if let Ok(latest_timestamp_millis) = self.current_timestamp_millis_since_epoch() {
+                            now = latest_timestamp_millis;
                         }
-                    }
-                    // First call in a time period
-                    Ordering::Greater => {
-                        self.step = 0;
+                        spin_loop();
                     }
                 }
-                self.time = now; // Update the most recent generation time
-                Ok((self.time << self.time_shift) | (self.node_id << self.node_shift) | (self.step))
             }
-            // Clock earlier than epoch directly reports an error
-            Err(_) => Err(SnowflakeError::EpochEarlierThanCurrentTime),
+            // First call in a time period
+            Ordering::Greater => {
+                self.sequence = 0;
+            }
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_generate_unique_ids() {
-        let mut snowflake = Snowflake::new(1).unwrap();
-
-        let id1 = snowflake.generate().unwrap();
-        let id2 = snowflake.generate().unwrap();
-        let id3 = snowflake.generate().unwrap();
-
-        assert_ne!(id1, id2);
-        assert_ne!(id1, id3);
-        assert_ne!(id2, id3);
+        // Update the most recent generation time
+        self.last_timestamp = now;
+        Ok((now << self.timestamp_shift) | (self.worker_id << self.worker_id_shift) | (self.sequence))
     }
 
-    #[test]
-    fn test_generate_in_order() {
-        let mut snowflake = Snowflake::new(1).unwrap();
-
-        let id1 = snowflake.generate().unwrap();
-        let id2 = snowflake.generate().unwrap();
-        let id3 = snowflake.generate().unwrap();
-
-        assert!(id1 < id2);
-        assert!(id2 < id3);
+    fn timestamp_millis() -> Result<u64, SnowflakeError> {
+        SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .map_err(|_| SnowflakeError::ClockMoveBackwards)?
+            .as_millis()
+            .try_into()
+            .map_err(|_| SnowflakeError::FailedConvertToMillis)
     }
 
-    #[test]
-    fn test_generate_with_multiple_nodes() {
-        let mut snowflake1 = Snowflake::new(1).unwrap();
-        let mut snowflake2 = Snowflake::new(2).unwrap();
-
-        let id1 = snowflake1.generate().unwrap();
-        let id2 = snowflake2.generate().unwrap();
-        let id3 = snowflake1.generate().unwrap();
-        let id4 = snowflake2.generate().unwrap();
-
-        assert_ne!(id1, id2);
-        assert_ne!(id1, id3);
-        assert_ne!(id1, id4);
-        assert_ne!(id2, id3);
-        assert_ne!(id2, id4);
-        assert_ne!(id3, id4);
-    }
-
-    #[test]
-    fn test_generate_with_timeout() {
-        let mut snowflake = Snowflake::with_config(1, 8, 100, EPOCH).unwrap();
-
-        let id1 = snowflake.generate().unwrap();
-        std::thread::sleep(Duration::from_millis(200));
-        let id2 = snowflake.generate().unwrap();
-
-        assert_ne!(id1, id2);
+    fn current_timestamp_millis_since_epoch(&self) -> Result<u64, SnowflakeError> {
+        let now = Self::timestamp_millis()?;
+        match now.cmp(&self.epoch) {
+            Ordering::Less => Err(SnowflakeError::ClockMoveBackwards),
+            _ => Ok(now - self.epoch),
+        }
     }
 }
